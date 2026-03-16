a6a4f7f718231cb2e7929f1c1fba694068d8ebd5
Tyoma <sobkalovt@bk.ru>
2026-03-16 15:46:52 +0300

    Add ProductConfiguration

---
+++ product.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using product.Domain.Models;
+
+namespace product.Infrastructure.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+        
+        builder.HasKey(p => p.Id);
+
+        builder
+            .Property(p => p.Id)
+            .HasColumnName("id");
+        
+        builder
+            .Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100)
+            .HasColumnName("name");
+        
+        builder
+            .Property(p=>p.Price)
+            .HasPrecision(18, 2)
+            .IsRequired()
+            .HasColumnName("price");
+        
+        builder
+            .Property(p => p.Quantity)
+            .IsRequired()
+            .HasColumnName("quantity");
+        
+        builder
+            .Property(p => p.CreatedAt)
+            .HasColumnName("created_at");
+
+        builder.Property(p => p.UpdatedAt)
+            .IsRequired(false)
+            .HasColumnName("updated_at");;
+    }
+}
