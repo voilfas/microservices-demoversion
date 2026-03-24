cddb96af0316b8f432d620d64b0c6f9993402304
Tyoma <sobkalovt@bk.ru>
2026-03-24 17:56:52 +0300

    Add serilog in GetProductById Handler

--- product.Application/UseCases/Commands/UpdateProduct/UpdateProductHandler.cs
+++ product.Application/UseCases/Commands/UpdateProduct/UpdateProductHandler.cs
@@ -1,7 +1,7 @@
 ﻿using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
 using product.Application.Interfaces;
-using product.Domain.Models;
 
 namespace product.Application.UseCases.Commands.UpdateProduct;
 
@@ -9,28 +9,41 @@ public class UpdateProductHandler
 {
     private readonly IProductRepository _productRepository;
     private readonly IDistributedCache _cache;
+    private readonly ILogger<UpdateProductHandler> _logger;
     
-    public UpdateProductHandler(IProductRepository productRepository, IDistributedCache cache)
+    public UpdateProductHandler(IProductRepository productRepository, IDistributedCache cache,  ILogger<UpdateProductHandler> logger)
     {
         _productRepository = productRepository;
         _cache = cache;
+        _logger = logger;
     }
 
     public async Task<Result<UpdateProductDto>> Handle(UpdateProductCommand command)
     {
+        _logger.LogInformation("Start updating product. " +
+                               "Name: {ProductName}, " +
+                               "Price: {ProductPrice}, " +
+                               "Quantity: {ProductQuantity}", command.Name, command.Price,  command.Quantity );
+        
         var product = await _productRepository.GetByIdAsync(command.Id);
         
         if (product is null)
             return Result.Failure<UpdateProductDto>("Product not found!");
 
         var updateProduct = product.UpdateProduct(command.Name, command.Price, command.Quantity);
-        
+
         if (updateProduct.IsFailure)
+        {
+            _logger.LogWarning("Could not update product. Reason: {Error}.",  updateProduct.Error);
+            
             return Result.Failure<UpdateProductDto>(updateProduct.Error);
+        }
         
         await _productRepository.SaveChangesAsync();
         
         await _cache.RemoveAsync($"product:{product.Id}");
+        
+        _logger.LogInformation("Product with ID: {ProductId} successfully updated in DB.", product.Id);
 
         var dto = new UpdateProductDto(
             product.Id, 
--- product.Application/UseCases/Queries/GetProductById/GetProductByIdHandler.cs
+++ product.Application/UseCases/Queries/GetProductById/GetProductByIdHandler.cs
@@ -2,6 +2,7 @@
 using CSharpFunctionalExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
 using product.Application.Interfaces;
 
 namespace product.Application.UseCases.Queries.GetProductById;
@@ -10,24 +11,33 @@ public class GetProductByIdHandler
 {
     private readonly IProductReadDbContext _dbContext;
     private readonly IDistributedCache _cache;
+    private readonly ILogger<GetProductByIdHandler> _logger;
 
-    public GetProductByIdHandler(IProductReadDbContext dbContext,  IDistributedCache cache)
+    public GetProductByIdHandler(IProductReadDbContext dbContext,  IDistributedCache cache, ILogger<GetProductByIdHandler> logger)
     {
         _dbContext = dbContext;
         _cache = cache;
+        _logger = logger;
     }
 
     public async Task<Result<DtoGetProductById>> Handle(GetProductByIdQuery query)
     {
         string cacheKey = $"product:{query.Id}";
         
+        _logger.LogInformation("Start searching for product in Redis db with ID: {query.Id}", query.Id);
+        
         var cachedProduct = await _cache.GetStringAsync(cacheKey);
 
         if (!string.IsNullOrEmpty(cachedProduct))
         {
             var productDto = JsonSerializer.Deserialize<DtoGetProductById>(cachedProduct);
+            
+            _logger.LogInformation("Found product with ID: {ProductId} in cache.", productDto?.Id);
+            
             return Result.Success(productDto);
         }
+        
+        _logger.LogInformation("Start searching for product in PostgreSQL db with ID: {query.Id}", query.Id);
         
        var product =  await _dbContext.Products
            .AsNoTracking()
@@ -39,9 +49,13 @@ public class GetProductByIdHandler
                p.Quantity, 
                p.CreatedAt))
            .FirstOrDefaultAsync();
-       
+
        if (product is null)
+       {
+           _logger.LogWarning("Product with ID: {ProductId} not found in PostgreSQL db.", query.Id);
+           
            return Result.Failure<DtoGetProductById>("Product not found");
+       }
 
        var options = new DistributedCacheEntryOptions
        {
@@ -50,6 +64,8 @@ public class GetProductByIdHandler
        
        var serializedProduct = JsonSerializer.Serialize(product);
        await _cache.SetStringAsync(cacheKey, serializedProduct, options);
+       
+       _logger.LogInformation("Product with ID: {ProductId} successfully founded in PostgreSQL and added in cache.", query.Id);
        
        return Result.Success(product);
     }
