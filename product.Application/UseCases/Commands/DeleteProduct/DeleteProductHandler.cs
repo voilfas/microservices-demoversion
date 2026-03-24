216c87213654be167e6b28e6841cc4d22ab314b0
Tyoma <sobkalovt@bk.ru>
2026-03-24 17:07:37 +0300

    Add serilog in DeleteProduct use case

--- product.Application/UseCases/Commands/DeleteProduct/DeleteProductHandler.cs
+++ product.Application/UseCases/Commands/DeleteProduct/DeleteProductHandler.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
 using product.Application.Interfaces;
 
 namespace product.Application.UseCases.Commands.DeleteProduct;
@@ -6,20 +7,31 @@ namespace product.Application.UseCases.Commands.DeleteProduct;
 public class DeleteProductHandler
 {
     private readonly IProductRepository _repository;
+    private readonly ILogger<DeleteProductHandler> _logger;
 
-    public DeleteProductHandler(IProductRepository repository)
+    public DeleteProductHandler(IProductRepository repository, ILogger<DeleteProductHandler> logger)
     {
         _repository = repository;
+        _logger = logger;
     }
 
     public async Task<Result> Handle(DeleteProductCommand command)
     {
+        _logger.LogInformation("Start deleting product. ID: {ProductId}.", command.Id);
+        
         var productDelete = await _repository.DeleteAsync(command.Id);
 
         if (productDelete.IsFailure)
+        {
+            _logger.LogWarning("Could not delete product with ID: {ProductId}. Reason: {Error}.", command.Id, productDelete.Error);
+            
             return Result.Failure("Product not found");
+        }
         
         await _repository.SaveChangesAsync();
+        
+        _logger.LogInformation("Product with ID: {ProductId} successfully deleted from DB.", command.Id);
+        
         return Result.Success();
     }
 }
